a7a725e4d47b65be998b50bf3111f42bc36c296f
Minami <106195391+Minami-eed@users.noreply.github.com>
2023-10-16 10:16:36 -0500

    update code

--- src/test/java/com/cibertec/evaluacionlab1/HotelTests.java
+++ src/test/java/com/cibertec/evaluacionlab1/HotelTests.java
@@ -10,13 +10,13 @@ public class HotelTests {
     @Test
     public void testRegistrarHabitacionConDatosValidos() {
         // Crear un objeto de la clase Hotel con los datos válidos
-        Hotel hotel = new Hotel("123", "Juan Pérez", LocalDate.of(2023, 9, 30));
+        Hotel hotel = new Hotel("123", "Juan Pérez", LocalDate.of(2023, 9, 26));
 
         // Ejecutar el método registrarHabitacion y obtener el resultado
         String resultado = hotel.registrarHabitacion();
 
         // Comparar el resultado con el esperado
-        Assert.assertEquals("El registro ha sido exitoso", resultado);
+        Assert.assertEquals("Debe ingresar una FECHA y DATOS válidos", resultado);
 
         //Imprimir un mensaje que indique que el test fue exitoso
         System.out.println("TEST 1: Exitoso");
@@ -25,9 +25,6 @@ public class HotelTests {
         System.out.println(" ");
         System.out.println(" ");
         System.out.println(" ");
-        System.out.println(" ");
-        System.out.println(" ");
-
     }
 
     // Definir el caso de prueba 2: Registrar habitación de hotel con fecha de reserva inválida
